1c5e794e01e730e8ce76b9176e42b5842838c2cf
Manjith Gunatilakagit <manjith155@yahoo.com>
2016-02-04 22:47:46 +1100

    initial

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,55 @@
-## Put comments here that give an overall description of what your
-## functions do
+## There are two functions, makeCacheMatrix() creates a matrix and 
+## and cache its contents. The other function, cacheSove()
+## creates an inverse matrix for a given matrix. If the inverse matrix
+## is already not in cached it creates a matrix and cached it. Otherwise
+## it will retunr cached inverse matrix
 
-## Write a short comment describing this function
+## # ---------------------------------------------------------------
+## This function creates a matrix and cached it. It has four sub functions
+## set(), get() , getInv() , setInv()
+## ---------------------------------------------------------------
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+  invMtx <- NULL
+  
+  set <- function(y) {
+    x <<- y
+    invMt <<- NULL
+    
+  }
+  get <- function() x
+  getInv <- function() invMtx
+  setInv <- function(inv) invMtx <<- inv
+  
+  list(set = set, get = get, setInv = setInv , getInv = getInv)
+  
 }
 
 
-## Write a short comment describing this function
+## Creates an inverse matrix if matrix is not chached, otherwise
+## it will return cached inverse matrix.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+  inv <- x$getInv()
+  
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  
+  mtx <- x$get()
+  inv <- solve(mtx)
+  
+  if (class(try(solve(mtx),silent=TRUE))=="matrix") {
+    inv <- solve(mtx)
+  }
+  else
+  {
+    message("cannot find inverse of the matrix, determinant is zero")
+    inv <- NULL
+  }   
+  x$setInv(inv)
+  inv
 }
